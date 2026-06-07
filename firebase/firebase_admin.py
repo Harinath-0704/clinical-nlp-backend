c6187817de81d4d4453f1c2e402166cca3a7fa83
Harinath <harinath0704@gmail.com>
2026-06-08 00:13:07 +0530

    fix indentation

--- firebase/firebase_admin.py
+++ firebase/firebase_admin.py
@@ -6,33 +6,22 @@ from flask import request, g, abort
 
 firebase_app = None
 
+
 def initialize_firebase():
     global firebase_app
     if firebase_admin._apps:
         return firebase_admin.get_app()
 
-    # Try JSON file first (local development)
     cred_path = os.path.join(os.path.dirname(__file__), 'serviceAccountKey.json')
+
     if os.path.exists(cred_path):
         cred = credentials.Certificate(cred_path)
     else:
-        # Use environment variables (production on Render)
-        private_key = os.getenv('FIREBASE_PRIVATE_KEY', '')
-if '\\n' in private_key:
-    private_key = private_key.replace('\\n', '\n')
-private_key = private_key.strip()
-        cred = credentials.Certificate({
-            'type': 'service_account',
-            'project_id': os.getenv('FIREBASE_PROJECT_ID'),
-            'private_key_id': os.getenv('FIREBASE_PRIVATE_KEY_ID'),
-            'private_key': private_key,
-            'client_email': os.getenv('FIREBASE_CLIENT_EMAIL'),
-            'client_id': os.getenv('FIREBASE_CLIENT_ID'),
-            'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-            'token_uri': 'https://oauth2.googleapis.com/token',
-            'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-            'client_x509_cert_url': os.getenv('FIREBASE_CLIENT_X509_CERT_URL'),
-        })
+        import base64
+        import json
+        b64 = os.getenv('FIREBASE_CREDENTIALS_BASE64', '')
+        cred_dict = json.loads(base64.b64decode(b64).decode())
+        cred = credentials.Certificate(cred_dict)
 
     firebase_app = firebase_admin.initialize_app(cred)
     return firebase_app
